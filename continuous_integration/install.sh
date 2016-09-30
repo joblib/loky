b61749998be089f545ca5b89e4b63c89e40e7c9b
Olivier Grisel <olivier.grisel@ensta.org>
2016-09-30 18:05:14 +0200

    Clean up in travis install script

--- continuous_integration/install.sh
+++ continuous_integration/install.sh
@@ -11,9 +11,11 @@ test $PYTHON == "python3" && PIP="pip3"
 
 ver=$($PYTHON -V 2>&1 | sed -e 's/Python \([23]\.[0-9]\).*/\1/' -e 's/\.//')
 echo "Testing for python $ver"
+
+# Make sure to install a recent pip version to be able to install numpy
+# with built-in openblas under linux
 $PIP install --upgrade pip
 $PIP uninstall -y numpy || echo "numpy not previously installed"
-$PYTHON -c "from pip.pep425tags import supported_tags; import pprint; pprint.pprint(supported_tags)"
 $PIP install psutil pytest numpy
 [ $ver -lt 33 ] && $PIP install faulthandler
 [ $ver -lt 33 ] && $PIP install futures
