27c08226ec41f9255d391a1ec03adc0bdf45a30f
Olivier Grisel <olivier.grisel@ensta.org>
2016-11-09 14:38:57 +0100

    Streamline CI to leverage tox

---
+++ continuous_integration/travis/runtests.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# This script is meant to be called by the "script" step defined in
+# .travis.yml. See http://docs.travis-ci.com/ for more details.
+
+# License: 3-clause BSD
+
+set -e
+$PYTHON --version
+echo $TOXENV
+
+# Run the tests and collect trace coverage data both in the subprocesses
+# and its subprocesses.
+COVERAGE_PROCESS_START="$TRAVIS_BUILD_DIR/.coveragerc" tox -- -v
