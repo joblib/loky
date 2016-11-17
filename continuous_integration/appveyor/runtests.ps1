5f74b3b8f4c0b57d5f285b472cd98fc8499afb1e
Olivier Grisel <olivier.grisel@ensta.org>
2016-11-17 15:56:05 +0100

    MAINT pass timeout to CI tox

--- continuous_integration/appveyor/runtests.ps1
+++ continuous_integration/appveyor/runtests.ps1
@@ -8,7 +8,7 @@ $VERSION=(27, 33, 34, 35)
 function TestPythonVersions () {
     Write-Host $PYTHON
     ForEach($ver in $VERSION){
-        python ./continuous_integration/appveyor/tox -e py$ver -- -vx
+        python ./continuous_integration/appveyor/tox -e py$ver -- -vx --timeout=5
         If( $LASTEXITCODE -ne 0){
             Exit 1
         }
