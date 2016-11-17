283ab9d3a9c5bdb1fee80463566a4d122cbcadda
Thomas Moreau <toma.moral@gmail.com>
2016-11-17 14:42:46 +0100

    FIX some small updates for codecov

--- loky/backend/fork_exec.py
+++ loky/backend/fork_exec.py
@@ -5,7 +5,7 @@ if sys.platform == "darwin" and sys.version_info < (3, 3):
      FileNotFoundError = OSError
 
 
-def close_fds(keep_fds):
+def close_fds(keep_fds):  # pragma: no cover
     keep_fds = set(keep_fds).union([0, 1, 2])
     try:
         open_fds = set([int(fd) for fd in os.listdir('/proc/self/fd')])
@@ -23,7 +23,7 @@ def close_fds(keep_fds):
 def fork_exec(cmd, keep_fds):
 
     pid = os.fork()
-    if pid == 0:
+    if pid == 0:  # pragma: no cover
         close_fds(keep_fds)
         os.execv(sys.executable, cmd)
     else:
