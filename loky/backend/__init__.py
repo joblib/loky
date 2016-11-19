aaaf45a5cdc5d6784152b8831a75a320fd97413c
tomMoral <thomas.moreau.2010@gmail.com>
2016-11-19 14:59:44 +0100

    FIX win32 import init

--- loky/backend/__init__.py
+++ loky/backend/__init__.py
@@ -1,5 +1,6 @@
 import sys
 
+import multiprocessing as mp
 from multiprocessing import Pipe
 if sys.platform != "win32":
     from .process import PosixLokyProcess as Process
@@ -8,31 +9,11 @@ if sys.platform != "win32":
         from .synchronize import *
         from .queues import Queue, SimpleQueue
     else:
-        import multiprocessing as mp
-        from multiprocessing import synchronize
         from multiprocessing import queues
         _ctx = mp.get_context('spawn')
 
         def Queue(*args, **kwargs):
             return queues.Queue(*args, ctx=_ctx, **kwargs)
-
-        def Semaphore(*args, **kwargs):
-            return synchronize.Semaphore(*args, ctx=_ctx, **kwargs)
-
-        def BoundedSemaphore(*args, **kwargs):
-            return synchronize.BoundedSemaphore(*args, ctx=_ctx, **kwargs)
-
-        def Lock(*args, **kwargs):
-            return synchronize.Lock(*args, ctx=_ctx, **kwargs)
-
-        def RLock(*args, **kwargs):
-            return synchronize.RLock(*args, ctx=_ctx, **kwargs)
-
-        def Event(*args, **kwargs):
-            return synchronize.Event(*args, ctx=_ctx, **kwargs)
-
-        def Condition(*args, **kwargs):
-            return synchronize.Condition(*args, ctx=_ctx, **kwargs)
 
         class LokyContext(mp.context.BaseContext):
             _name = 'loky'
@@ -41,18 +22,38 @@ if sys.platform != "win32":
 
 else:
     from multiprocessing import Process
-    from multiprocessing.synchronize import *
-    if sys.version_info[:2] > (3, 3):
+    if sys.version_info[:2] < (3, 4):
+        from .queues import SimpleQueue, Queue
+        from multiprocessing.synchronize import *
+    else:
         from multiprocessing import SimpleQueue, Queue
-        from multiprocessing import context, get_context
 
-        _ctx = get_context("spawn")
+        _ctx = mp.get_context("spawn")
 
         def LokyContext():
             return _ctx
-        context._concrete_contexts['loky'] = LokyContext()
-    else:
-        from .queues import SimpleQueue, Queue
+        mp.context._concrete_contexts['loky'] = LokyContext()
+
+if sys.version_info > (3, 4):
+    from multiprocessing import synchronize
+
+    def Semaphore(*args, **kwargs):
+        return synchronize.Semaphore(*args, ctx=_ctx, **kwargs)
+
+    def BoundedSemaphore(*args, **kwargs):
+        return synchronize.BoundedSemaphore(*args, ctx=_ctx, **kwargs)
+
+    def Lock(*args, **kwargs):
+        return synchronize.Lock(*args, ctx=_ctx, **kwargs)
+
+    def RLock(*args, **kwargs):
+        return synchronize.RLock(*args, ctx=_ctx, **kwargs)
+
+    def Event(*args, **kwargs):
+        return synchronize.Event(*args, ctx=_ctx, **kwargs)
+
+    def Condition(*args, **kwargs):
+        return synchronize.Condition(*args, ctx=_ctx, **kwargs)
 
 __all__ = ["Process", "Queue", "SimpleQueue", "Lock", "RLock", "Semaphore",
            "BoundedSemaphore", "Condition", "Event", "Pipe"]
