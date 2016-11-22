eb58efacd28dfa185cfece67aa64eae241f62617
Thomas Moreau <toma.moral@gmail.com>
2016-11-22 14:54:52 +0100

    WIP fix openmp osx

--- continuous_integration/build_test_ext.sh
+++ continuous_integration/build_test_ext.sh
@@ -1,5 +1,10 @@
 
 cd tests/_openmp
 COVERAGE_PROCESS_START=''
-test $PYENV != py36 && python setup.py build_ext -i
-cd ..
+if [[ ${TRAVIS_OS_NAME} = osx ]]; then
+    # Since default gcc on osx is just a front-end for LLVM
+    CC=gcc-4.8 python setup.py build_ext -i
+elif [[ $PYENV != py36 ]]; then
+	python setup.py build_ext -i
+fi
+cd ../..
