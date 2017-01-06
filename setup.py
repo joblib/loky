babe7fd88f82b7345cb3988f2755960dc246fcaf
Thomas Moreau <toma.moral@gmail.com>
2017-01-06 17:21:33 +0100

    ENH clean repo

--- setup.py
+++ setup.py
@@ -1,6 +1,41 @@
+import os
+import shutil
 from setuptools import setup, find_packages
+from distutils.command.clean import clean as Clean
 
 packages = find_packages(exclude=['tests', 'tests._openmp', 'benchmark'])
+
+
+# Custom clean command to remove build artifacts
+
+class CleanCommand(Clean):
+    description = "Remove build artifacts from the source tree"
+
+    def run(self):
+        Clean.run(self)
+        # Remove c files if we are not within a sdist package
+        cwd = os.path.abspath(os.path.dirname(__file__))
+        remove_c_files = not os.path.exists(os.path.join(cwd, 'PKG-INFO'))
+        if remove_c_files:
+            print('Will remove generated .c files')
+        if os.path.exists('build'):
+            shutil.rmtree('build')
+        for dirpath, dirnames, filenames in os.walk('.'):
+            for filename in filenames:
+                if any(filename.endswith(suffix) for suffix in
+                       (".so", ".pyd", ".dll", ".pyc")):
+                    os.unlink(os.path.join(dirpath, filename))
+                    continue
+                extension = os.path.splitext(filename)[1]
+                if remove_c_files and extension in ['.c', '.cpp']:
+                    pyx_file = str.replace(filename, extension, '.pyx')
+                    if os.path.exists(os.path.join(dirpath, pyx_file)):
+                        os.unlink(os.path.join(dirpath, filename))
+            for dirname in dirnames:
+                if dirname == '__pycache__':
+                    shutil.rmtree(os.path.join(dirpath, dirname))
+
+cmdclass = {'clean': CleanCommand}
 
 setup(
     name='loky',
@@ -30,9 +65,6 @@ setup(
         'Topic :: Utilities',
         'Topic :: Software Development :: Libraries',
     ],
+    cmdclass=cmdclass,
     platforms='any'
-
-
-
-
 )
