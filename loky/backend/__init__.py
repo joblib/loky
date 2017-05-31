375955d688b2f8bcfd49b607aae07b77b187bcf7
Thomas Moreau <toma.moral@gmail.com>
2017-05-31 10:22:57 +0200

    CLN fix staticmethod usage

--- loky/backend/__init__.py
+++ loky/backend/__init__.py
@@ -7,13 +7,12 @@ LOKY_PICKLER = os.environ.get("LOKY_PICKLER")
 
 if sys.version_info > (3, 4):
 
-    @staticmethod
     def _make_name():
         name = '/loky-%i-%s' % (os.getpid(), next(synchronize.SemLock._rand))
         return name
 
     # monkey patch the name creation for multiprocessing
     from multiprocessing import synchronize
-    synchronize.SemLock._make_name = _make_name
+    synchronize.SemLock._make_name = staticmethod(_make_name)
 
 __all__ = ["get_context"]
