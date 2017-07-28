3548ba9743bb025b09ac76bbc88de74daa0d8f91
Thomas Moreau <toma.moral@gmail.com>
2017-07-28 16:33:53 +0200

    FIX restrict number of process in shutdown sys exit

--- tests/_test_process_executor.py
+++ tests/_test_process_executor.py
@@ -92,12 +92,12 @@ class ExecutorShutdownTest:
 
     def test_shutdown_with_pickle_error(self):
         self.executor.shutdown()
-        with self.executor_type() as e:
+        with self.executor_type(max_workers=4) as e:
             e.submit(id, ErrorAtPickle())
 
     def test_shutdown_with_sys_exit_at_pickle(self):
         self.executor.shutdown()
-        with self.executor_type() as e:
+        with self.executor_type(max_workers=4) as e:
             e.submit(id, ExitAtPickle())
 
     def test_interpreter_shutdown(self):
