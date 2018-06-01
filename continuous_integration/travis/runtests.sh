be45caec928f2d547be3c9fcb3e9159f785a7436
tomMoral <thomas.moreau.2010@gmail.com>
2018-06-01 14:35:03 +0200

    CI run joblib test in travis

--- continuous_integration/travis/runtests.sh
+++ continuous_integration/travis/runtests.sh
@@ -19,3 +19,15 @@ fi
 # and its subprocesses.
 COVERAGE_PROCESS_START="$TRAVIS_BUILD_DIR/.coveragerc" tox -- -vl \
         --timeout=30 --maxfail=5
+
+
+if [ "$JOBLIB_TESTS" == "true" ]; then
+    # Install joblib from pip, patch it to use this version of loky
+    # and run the joblib tests with pytest.
+    pip install joblib
+    export JOBLIB=`python -c "import joblib; print(joblib.__path__[0])"`
+    cp $TRAVIS_BUILD_DIR/continuous_integration/travis/copy_loky.sh $JOBLIB/externals
+    (cd $JOBLIB/externals && bash copy_loky.sh $TRAVIS_BUILD_DIR)
+    cp $TRAVIS_BUILD_DIR/continuous_integration/travis/conftest.py $JOBLIB/..
+    pytest -vl --ignore $JOBLIB/externals --pyargs joblib
+fi
