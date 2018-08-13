2b9114adef2eced210a1dea54513952610d4aebe
tomMoral <thomas.moreau.2010@gmail.com>
2018-08-13 10:42:41 +0200

    FIX  appveyor ci launch script

--- continuous_integration/appveyor/runtests.ps1
+++ continuous_integration/appveyor/runtests.ps1
@@ -4,17 +4,20 @@
 # License: 3-clause BSD
 
 $VERSION=(36, 34, 27)
+$TOX_CMD = "python ./continuous_integration/appveyor/tox"
 
 function TestPythonVersions () {
     Write-Host $PYTHON
     ForEach($ver in $VERSION){
         $env:TOXPYTHON = "C:\Python$ver$env:PYTHON_ARCH_SUFFIX\python.exe"
         Write-Host $env:TOXPYTHON
-        $PYTEST_ARGS = "-vlx --timeout=50"
+        $PYTEST_ARGS = "-vxl --timeout=50"
         If( $env:PYTHON_ARCH -eq 32){
             $PYTEST_ARGS = "$PYTEST_ARGS --no-memory"
         }
-        python ./continuous_integration/appveyor/tox -e py$ver -- $PYTEST_ARGS
+        # Launch the tox command for the correct python version. We use iex to
+        # correctly pass PYTEST_ARGS, which are parsed as files otherwise.
+        iex "$TOX_CMD -e py$ver -- $PYTEST_ARGS"
         If( $LASTEXITCODE -ne 0){
             Exit 1
         }
