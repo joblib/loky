a3587ac1ab6377869f668977e6f62a3da703a2cf
tomMoral <thomas.moreau.2010@gmail.com>
2019-03-01 18:01:15 +0100

    FIX coverage in azure

--- continuous_integration/azure/test_script.cmd
+++ continuous_integration/azure/test_script.cmd
@@ -2,5 +2,7 @@ set DEFAULT_PYTEST_ARGS=-vlx --skip-high-memory
 
 call activate %VIRTUALENV%
 
+python continuous_integration/install_coverage_subprocess_pth.py
+
 pytest --junitxml=%JUNITXML% %DEFAULT_PYTEST_ARGS%
 coverage combine --append
