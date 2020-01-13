3490ba5ed845408b51eb06a48f5a5d22f0ac44e5
Pierre Glaser <pierreglaser@msn.com>
2020-01-14 00:12:36 +0100

    try with set

--- continuous_integration/azure/test_script.cmd
+++ continuous_integration/azure/test_script.cmd
@@ -4,6 +4,6 @@ call activate %VIRTUALENV%
 
 python continuous_integration/install_coverage_subprocess_pth.py
 
-setx COVERAGE_STORAGE=json
+set COVERAGE_STORAGE=json
 
 pytest --junitxml=%JUNITXML% %DEFAULT_PYTEST_ARGS%
