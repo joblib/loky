3ccde6f654c11a931ea3f34878a0cbf550a9029c
Pierre Glaser <pierreglaser@msn.com>
2020-01-14 15:05:53 +0100

    increase upload_codecov.[sh, cmd] verbosity

--- continuous_integration/azure/upload_codecov.cmd
+++ continuous_integration/azure/upload_codecov.cmd
@@ -1,4 +1,9 @@
 call activate %VIRTUALENV%
+where pip
+
 pip install codecov
+where codecov
+where coverage
+
 coverage combine --append
 codecov || echo "codecov upload failed"
--- continuous_integration/azure/upload_codecov.sh
+++ continuous_integration/azure/upload_codecov.sh
@@ -2,11 +2,13 @@
 
 set -e
 
-conda init bash
 conda activate $VIRTUALENV
+which pip
 
 pip install coverage
 pip install codecov
+which codecov
+which coverage
 
 coverage combine --append
 codecov || echo "codecov upload failed"
