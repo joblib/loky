3999420bc423563edfae5324ce4cd237d6c9639e
Pierre Glaser <pierreglaser@msn.com>
2020-02-10 19:23:05 +0100

    restore Python 3.8 CI entries and fix a test (#232)

--- tests/_test_process_executor.py
+++ tests/_test_process_executor.py
@@ -22,6 +22,7 @@ import gc
 import sys
 import time
 import shutil
+import platform
 import pytest
 import weakref
 import tempfile
@@ -535,9 +536,20 @@ class ExecutorTest:
         self.executor.map(str, [2] * (self.worker_count + 1))
         self.executor.shutdown()
 
+
+    @pytest.mark.skipif(
+            platform.python_implementation() != "CPython" or
+            (sys.version_info >= (3, 8, 0) and sys.version_info < (3, 8, 2)),
+            reason="Underlying bug fixed upstream starting Python 3.8.2")
     def test_no_stale_references(self):
         # Issue #16284: check that the executors don't unnecessarily hang onto
         # references.
+
+        # This test has to be skipped on early Python 3.8 versions because of a
+        # low-level reference cycle inside the pickle module for early versions
+        # of Python 3.8 preventing stale references from being collected. See
+        # cloudpipe/cloudpickle#327 as well as
+        # https://bugs.python.org/issue39492
         my_object = MyObject()
         collect = threading.Event()
         _ = weakref.ref(my_object, lambda obj: collect.set())  # noqa
@@ -793,11 +805,19 @@ class ExecutorTest:
                                  ctx=self.context)
 
     @pytest.mark.high_memory
-    @pytest.mark.skipif(sys.version_info < (3, 4),
-                        reason="Limitation only present for python3")
-    def test_failure_on_large_data_send(self):
+    @pytest.mark.skipif(
+            sys.version_info[:2] < (3, 8),
+            reason="These Pythons cannot pickle objects of size > 2 ** 31GB")
+    def test_no_failure_on_large_data_send(self):
         data = b'\x00' * int(2.2e9)
+        self.executor.submit(id, data).result()
 
+    @pytest.mark.high_memory
+    @pytest.mark.skipif(
+            sys.version_info[:2] >= (3, 8),
+            reason="These Pythons can pickle objects of size > 2 ** 31GB")
+    def test_expected_failure_on_large_data_send(self):
+        data = b'\x00' * int(2.2e9)
         with pytest.raises(RuntimeError):
             self.executor.submit(id, data).result()
 
