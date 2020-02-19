1bc2d43e338d96177f5a7b95b134fd88c191c5ee
Olivier Grisel <olivier.grisel@ensta.org>
2020-02-19 17:37:56 +0100

    Fix some code style in the example

--- examples/reuse_executor.py
+++ examples/reuse_executor.py
@@ -8,14 +8,14 @@ The factory ``get_reusable_executor`` provides an executor. As long as this
 executor is not in a broken state, it is reused for all the computation.
 """
 import os
-import argparse
 from loky import get_reusable_executor
 
 
 def func_async(i):
     import os
     pid = os.getpid()
-    return (2*i, pid)
+    return (2 * i, pid)
+
 
 def test_1():
     executor = get_reusable_executor(max_workers=1)
@@ -31,6 +31,7 @@ def test_3():
     executor = get_reusable_executor(max_workers=1)
     return executor.submit(func_async, 3)
 
+
 f1 = test_1()
 f2 = test_2()
 f3 = test_3()
@@ -38,7 +39,7 @@ f3 = test_3()
 main_pid = os.getpid()
 results = [f1.result(), f2.result(), f3.result()]
 
-pids = [v[1] for v in results]
+pids = [pid for _, pid in results]
 
 for i, (val, pid) in enumerate(results):
     assert val == 2 * (i + 1)
