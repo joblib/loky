d0f4320450ec45a1d55a9ea8d9c22a0056725924
pmla <peter.mahler.larsen@gmail.com>
2020-07-03 19:19:33 +0200

    document problems with resizing

--- loky/worker_id.py
+++ loky/worker_id.py
@@ -6,7 +6,8 @@ def get_worker_id():
     with `max_workers=n`, the worker ID is in the range [0..n). This is suited
     for reuse of persistent objects such as GPU IDs. This function only works
     at the first level of parallelization (i.e. not for nested
-    parallelization). Returns -1 on failure.
+    parallelization). Resizing the `ReusableExectutor` will result in
+    unpredictable return values. Returns -1 on failure.
     """
     wid = os.environ.get('LOKY_WORKER_ID', None)
     if wid is None:
