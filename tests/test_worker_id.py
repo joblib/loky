4be8b0485a560cf14efb821be0d831f1f54d0d61
pmla <peter.mahler.larsen@gmail.com>
2020-07-03 19:27:46 +0200

    added timeout test

--- tests/test_worker_id.py
+++ tests/test_worker_id.py
@@ -1,4 +1,3 @@
-import os
 import time
 import pytest
 import numpy as np
@@ -6,9 +5,10 @@ from collections import defaultdict
 from loky import get_reusable_executor, get_worker_id
 
 
-def random_sleep(k):
+def random_sleep(args):
+    k, max_duration = args
     rng = np.random.RandomState(seed=k)
-    duration = rng.uniform(0, 0.05)
+    duration = rng.uniform(0, max_duration)
     t0 = time.time()
     time.sleep(duration)
     t1 = time.time()
@@ -16,11 +16,14 @@ def random_sleep(k):
     return (wid, t0, t1)
 
 
-def test_worker_ids():
+@pytest.mark.parametrize("max_duration,timeout,kmax", [(0.05, 2, 100),
+                                                       (1, 0.01, 4)])
+def test_worker_ids(max_duration, timeout, kmax):
     """Test that worker IDs are always unique, with re-use over time"""
     num_workers = 4
     executor = get_reusable_executor(max_workers=num_workers, timeout=2)
-    results = executor.map(random_sleep, range(100))
+    results = executor.map(random_sleep, [(k, max_duration)
+                                          for k in range(kmax)])
 
     all_intervals = defaultdict(list)
     for wid, t0, t1 in results:
