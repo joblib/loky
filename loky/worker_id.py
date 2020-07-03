93927db89219fea68cb0caf198a17859b5ade4ff
pmla <peter.mahler.larsen@gmail.com>
2020-07-03 18:43:34 +0200

    docstring

--- loky/worker_id.py
+++ loky/worker_id.py
@@ -2,6 +2,12 @@ import os
 
 
 def get_worker_id():
+    """Get the worker ID of the current process. For a `ReusableExectutor`
+    with `max_workers=n`, the worker ID is in the range [0..n). This is suited
+    for reuse of persistent objects such as GPU IDs. This function only works
+    at the first level of parallelization (i.e. not for nested
+    parallelization). Returns -1 on failure.
+    """
     wid = os.environ.get('LOKY_WORKER_ID', None)
     if wid is None:
         return -1
