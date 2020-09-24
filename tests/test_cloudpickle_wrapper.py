5f8c2fdc27f04b5413a07a98c75fb829284d86ce
Olivier Grisel <olivier.grisel@ensta.org>
2020-09-24 18:09:28 +0200

    [MRG] Fix custom reducers registered in dispatch_table handled by a member descriptor  (#272)

--- tests/test_cloudpickle_wrapper.py
+++ tests/test_cloudpickle_wrapper.py
@@ -1,14 +1,50 @@
 import os
 import sys
 import pytest
+import io
+from pickle import loads
 from tempfile import mkstemp
 from loky import set_loky_pickler
+from loky.backend.reduction import get_loky_pickler
 
 
 from .utils import check_subprocess_call
 
 
 class TestCloudpickleWrapper:
+
+    @pytest.mark.skipif(sys.version_info[0] == 2, reason="Python 3 or later")
+    def test_isolated_pickler_dispatch_tables(self):
+        class A:
+            pass
+
+        class B:
+            pass
+
+        iob1 = io.BytesIO()
+        p1 = get_loky_pickler()(iob1,
+                                reducers={A: lambda obj: (int, (42,))})
+        iob2 = io.BytesIO()
+        p2 = get_loky_pickler()(iob2,
+                                reducers={B: lambda obj: (int, (42,))})
+
+        assert p1.dispatch_table is not p2.dispatch_table
+
+        assert A in p1.dispatch_table
+        assert A not in p2.dispatch_table
+
+        assert B in p2.dispatch_table
+        assert B not in p1.dispatch_table
+
+        p1.dump((A(), B()))
+        a, b = loads(iob1.getvalue())
+        assert a == 42
+        assert isinstance(b, B)
+
+        p2.dump((A(), B()))
+        a, b = loads(iob2.getvalue())
+        assert isinstance(a, A)
+        assert b == 42
 
     def test_serialization_function_from_main(self):
         # check that the init_main_module parameter works properly
