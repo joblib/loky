b3c55f1ac1a0aafeb31f90f5d86c0fc38a46c072
jeremie du boisberranger <jeremiedbb@yahoo.fr>
2022-02-03 15:17:22 +0100

    mount all loky

--- tests/test_loky_module.py
+++ tests/test_loky_module.py
@@ -74,13 +74,14 @@ def test_cpu_count_cfs_limit():
     if docker_bin is None:
         raise pytest.skip()
 
-    loky_path = os.path.abspath(os.path.dirname(loky.__file__))
+    loky_module_path = os.path.abspath(os.path.dirname(loky.__file__))
+    loky_project_path = os.path.abspath(os.path.join(loky_module_path, os.pardir))
 
     # The following will always run using the Python 3.7 docker image.
     # We mount the loky source as /loky inside the container,
     # so it can be imported when running commands under /
     res = check_output(
-        f"{docker_bin} run --rm --cpus 0.5 -v {loky_path}:/loky python:3.7 "
+        f"{docker_bin} run --rm --cpus 0.5 -v {loky_project_path}:/loky python:3.7 "
         f"/bin/bash -c 'pip install -e /loky ; "
         f"python -c \"{cpu_count_cmd.format(args='')}\"'",
         shell=True
