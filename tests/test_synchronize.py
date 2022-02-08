fa2937a546d28c6a8c93ed3c3be9edd1789f9ffe
Olivier Grisel <olivier.grisel@ensta.org>
2022-02-08 11:46:34 +0100

    Make test_semlock_failure independent of previous runs

--- tests/test_synchronize.py
+++ tests/test_synchronize.py
@@ -21,11 +21,15 @@ def test_semlock_failure():
     from loky.backend.synchronize import SemLock, sem_unlink
 
     name = "loky-test-semlock"
-    sl = SemLock(0, 1, 1, name=name)
+    try:
+        sl = SemLock(0, 1, 1, name=name)
 
-    with pytest.raises(FileExistsError):
-        SemLock(0, 1, 1, name=name)
-    sem_unlink(sl.name)
+        with pytest.raises(FileExistsError):
+            SemLock(0, 1, 1, name=name)
+    finally:
+        # Always clean-up the test semaphore to make this test independent of
+        # previous runs (successful or not).
+        sem_unlink(sl.name)
 
     with pytest.raises(FileNotFoundError):
         sl._semlock._rebuild(0, 0, 0, name)
