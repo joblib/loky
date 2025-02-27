36e9c402f6c0201a66b348c3a9fbe199668e2c70
Jérémie du Boisberranger <jeremie@probabl.ai>
2025-02-27 18:35:01 +0100

    iter

--- continuous_integration/install.sh
+++ continuous_integration/install.sh
@@ -6,12 +6,14 @@ set -xe
 
 # Create new conda env
 conda config --set solver libmamba
-to_install="python=$PYTHON_VERSION pip pytest numpy tblib viztracer"
+to_install="python=$PYTHON_VERSION pip numpy tblib viztracer"
 conda create -n testenv --yes -c conda-forge $to_install
 conda activate testenv
 
-# Install pytest timeout to fasten failure in deadlocking tests
-PIP_INSTALL_PACKAGES="pytest-timeout coverage pytest-cov"
+if [[ -z "$JOBLIB_TESTS" ]]; then
+    # Install pytest timeout to fasten failure in deadlocking tests
+    PIP_INSTALL_PACKAGES="pytest pytest-timeout coverage pytest-cov"
+fi
 
 if [[ -z "$NO_PSUTIL" ]]; then
     PIP_INSTALL_PACKAGES="$PIP_INSTALL_PACKAGES psutil"
