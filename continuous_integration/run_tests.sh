2bbfd06a8247e954d9da98665b2a747b1c192b7b
Jérémie du Boisberranger <jeremie@probabl.ai>
2025-02-27 17:56:22 +0100

    iter

--- continuous_integration/run_tests.sh
+++ continuous_integration/run_tests.sh
@@ -41,7 +41,7 @@ else
     # Run the tests and collect trace coverage data both in the subprocesses
     # and its subprocesses.
     if [[ "$RUN_MEMORY" != "true" ]]; then
-        PYTEST_ARGS = "$PYTEST_ARGS --skip-high-memory"
+        PYTEST_ARGS="$PYTEST_ARGS --skip-high-memory"
     fi
 
     pytest loky $PYTEST_ARGS
