b55982cf0932d93283ecb242f1a7b132fb34b617
Jérémie du Boisberranger <jeremie@probabl.ai>
2025-02-28 16:40:38 +0100

    add Python 3.13 jobs

--- setup.py
+++ setup.py
@@ -78,6 +78,8 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
         "Topic :: Scientific/Engineering",
         "Topic :: Utilities",
         "Topic :: Software Development :: Libraries",
