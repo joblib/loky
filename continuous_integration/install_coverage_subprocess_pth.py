bce94b4c54b6958e1a4b5e90aba883cbd7fae779
Olivier Grisel <olivier.grisel@ensta.org>
2025-03-07 14:35:05 +0100

    comment for platlib

--- continuous_integration/install_coverage_subprocess_pth.py
+++ continuous_integration/install_coverage_subprocess_pth.py
@@ -9,6 +9,8 @@ FILE_CONTENT = """\
 import coverage; coverage.process_startup()
 """
 
+# get_path("platlib") returns the location of the `site-packages` folder:
+# https://docs.python.org/3/library/sysconfig.html
 filename = op.join(get_path("platlib"), "coverage_subprocess.pth")
 with open(filename, mode="w") as f:
     f.write(FILE_CONTENT)
