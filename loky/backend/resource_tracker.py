9f15b55ac3df3665b8ca0abdb8ec29734a65cc0a
Itamar Turner-Trauring <itamar@itamarst.org>
2026-08-05 09:25:47 -0400

    Revert workaround that just increases the chance of other problems

--- loky/backend/resource_tracker.py
+++ loky/backend/resource_tracker.py
@@ -241,12 +241,7 @@ class ResourceTracker(_ResourceTracker):
         if not hasattr(_ResourceTracker, "__del__"):
             return
         try:
-            if sys.version_info[:2] == (3, 13):
-                # __del__ calls this with a timeout, which doesn't work on
-                # Windows:
-                super()._stop(use_blocking_lock=False)
-            else:
-                super().__del__()
+            super().__del__()
         except ChildProcessError:
             pass
 
