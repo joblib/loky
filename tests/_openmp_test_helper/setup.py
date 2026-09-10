312aa9e3b35489fa1d86819c1a253662d011e249
Stefanie Senger <stefanie.senger@posteo.de>
2026-09-10 12:26:20 +0200

    add openmp flags

--- tests/_openmp_test_helper/setup.py
+++ tests/_openmp_test_helper/setup.py
@@ -11,7 +11,10 @@ from setuptools.extension import Extension
 #     os.environ["CC"] = "gcc-13"
 #     os.environ["CXX"] = "g++-13"
 
-if sys.platform != "win32":
+if sys.platform == "darwin":
+    extra_compile_args = ["-ffast-math", "-Xpreprocessor", "-fopenmp"]
+    extra_link_args = ["-lomp"]
+elif sys.platform != "win32":
     extra_compile_args = ["-ffast-math", "-fopenmp"]
     extra_link_args = ["-fopenmp"]
 else:
