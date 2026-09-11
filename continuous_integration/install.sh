de1e3db8b4bf35a90acbec1a7b0fa056b95f9781
Stefanie Senger <stefanie.senger@posteo.de>
2026-09-11 11:00:46 +0200

    set freethreading_compatible = True marker

--- continuous_integration/install.sh
+++ continuous_integration/install.sh
@@ -39,7 +39,7 @@ pip install $PIP_INSTALL_PACKAGES
 
 pip install -v .
 
-if [[ -z "$JOBLIB_TESTS" && "$FREE_THREADING" != "true" ]]; then
+if [[ -z "$JOBLIB_TESTS" ]]; then
     pip install cython setuptools
     cd tests/_openmp_test_helper
     python setup.py build_ext -i || echo 'No openmp'
--- tests/_openmp_test_helper/parallel_sum.pyx
+++ tests/_openmp_test_helper/parallel_sum.pyx
@@ -1,3 +1,5 @@
+# cython: freethreading_compatible = True
+
 import cython
 cimport openmp
 from cython.parallel import prange
